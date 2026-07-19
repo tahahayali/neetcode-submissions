40be383978468948f9a412e74c4a822e10c01fa5
tahahayali <123418904+tahahayali@users.noreply.github.com>
2026-07-18 20:05:49 -0400

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        start = {'{' : 1, '[' : 2, '(' : 3}
+        close = {'}' : 1, ']' : 2, ')' : 3}
+        n = len(s)
+        if s[0] in close or s[-1] in start or (n % 2) != 0:
+            return False
+        stack = []
+        for c in s:
+            if start.get(c):
+                stack.append(c)
+            elif c in close:
+                same = stack.pop()
+                if close[c] != start[same]:
+                    return False
+        if len(stack) != 0:
+            return False
+        return True
